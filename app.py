6acceb90854fe5adf7be580814c5083e2b99db2f
Bruce <purushoth170288@gmail.com>
2024-11-21 20:20:46 +0530

    new dock comit

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def run():
-    return "<h1>Rioyich tenkai</h1>"
+    return "<h1>Rioyich tenkai - domain Expansion</h1>"
 app.run('0.0.0.0',5000)
