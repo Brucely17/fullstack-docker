3abbcb30db8e9b1be5ad4572b8bb393cbddb7781
Brucely17 <purushoth170288@gmail.com>
2025-01-06 12:15:51 +0530

    small change

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def run():
     if request.method == 'GET':
-        # Handle GET request
+        
         return """
                 <h1>Rioyich Tenkai - Domain Expansion (G)</h1>
                 <br> 
                 <h2>Gojo Sataraou</h2> <br><h2>Sukuna</h2>
+                <br>
+                <h2>Arigatho Osaimasu</h2>
                 """
     elif request.method == 'POST':
-        # Handle POST request
-        data = request.get_json()  # Parse JSON data from the POST request
+        
+        data = request.get_json()  
         if not data:
             return jsonify({"error": "No JSON data received"}), 400
         return jsonify({
